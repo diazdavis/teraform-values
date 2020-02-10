951dde5c8392d4a3dc4d3de6ba65ddd7cbcbb706
Diaz Davis <diazcddiaz@gmail.com>
2020-02-10 16:01:51 +0530

    Update readdata.sh

--- readdata.sh
+++ readdata.sh
@@ -4,6 +4,50 @@ region= $region
 export region
 printenv region
 
-echo "what is your region..?"
-read region
-echo "hello $region"
+environment= $environment
+export environment
+printenv environment
+
+project= $project
+export project
+printenv project
+
+vpc_cidr= $vpc_cidr
+export vpc_cidr
+printenv vpc_cidr
+
+nat_count= $nat_count
+export nat_count
+printenv nat_count
+
+public_subnet_cidr= $public_subnet_cidr
+export public_subnet_cidr
+printenv public_subnet_cidr
+
+private_subnet_cidr= $private_subnet_cidr
+export private_subnet_cidr
+printenv private_subnet_cidr
+
+bastion_instance_type= $bastion_instance_type
+export bastion_instance_type
+printenv bastion_instance_type
+
+enable_bastion= $enable_bastion
+export enable_bastion
+printenv enable_bastion
+
+bastion_instance_type= $bastion_instance_type
+export bastion_instance_type
+printenv bastion_instance_type
+
+bastion_kp_sudo_pub_key= $bastion_kp_sudo_pub_key
+export bastion_kp_sudo_pub_key
+printenv bastion_kp_sudo_pub_key
+
+bastion_kp_developer_pub_key= $bastion_kp_developer_pub_key
+export bastion_kp_developer_pub_key
+printenv bastion_kp_developer_pub_key
+
+bastion_ssh_port= $bastion_ssh_port
+export bastion_ssh_port
+printenv bastion_ssh_port
